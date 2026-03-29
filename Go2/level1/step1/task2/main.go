1c200b1b58d26d4ba83b9b5a3e628ff23731cd44
Morvolz <kola311@yandex.by>
2026-03-29 16:02:56 +0300

    AC

--- Go2/level1/step1/task2/main.go
+++ Go2/level1/step1/task2/main.go
@@ -1,1 +1,20 @@
-package task2
+package main
+
+import (
+	"io"
+)
+
+// type r interface {
+// 	Read(p []byte) (n int, err error)
+// }
+
+func ReadString(r io.Reader) (string, error) {
+	s, err := io.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	return string(s), err
+}
+func main() {
+
+}
