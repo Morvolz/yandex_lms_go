eabf1a4970f91aac6961a9b99478661750f11337
Morvolz <kola311@yandex.by>
2026-03-29 16:53:11 +0300

    AC

--- Go2/level1/task5/main.go
+++ Go2/level1/task5/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"bytes"
+	"io"
+)
+
+func Contains(r io.Reader, seq []byte) (bool, error) {
+	buffer := make([]byte, 4096)
+
+	_, err := r.Read(buffer)
+
+	if err != nil {
+		return false, err
+	}
+
+	idx := bytes.Index(buffer, seq)
+
+	if idx == -1 {
+		return false, nil
+	} else {
+		return true, nil
+	}
+}
