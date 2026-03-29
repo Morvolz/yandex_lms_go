54d90bbfaad0c3af57f3ce7de7032e8fbe53257a
Morvolz <kola311@yandex.by>
2026-03-29 15:52:01 +0300

    AC

--- Go2/level1/step1/task1/main.go
+++ Go2/level1/step1/task1/main.go
@@ -1,7 +1,14 @@
 package main
 
-type Reader interface {
-	Read(p []byte) (n int, err error)
+import "io"
+
+type w interface {
+	Write(p []byte) (n int, err error)
+}
+
+func WriteString(s string, w io.Writer) error {
+	_, err := w.Write([]byte(s))
+	return err
 }
 
 func main() {
