acdee693d765b6c4c3a585f9b5e57e260f3ac547
Morvolz <kola311@yandex.by>
2026-03-30 18:59:47 +0300

    AC

--- Go2/level1/step2/task2/main.go
+++ Go2/level1/step2/task2/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 )
 
@@ -11,14 +10,14 @@ func LineByNum(inputFilename string, lineNum int) string {
 	fileScanner := bufio.NewScanner(f)
 	var i int
 	for fileScanner.Scan() {
-		i++
-		// fmt.Print(fileScanner.Text(), "\n")
 		if i == lineNum {
 			return fileScanner.Text()
 		}
+		i++
 	}
 	return ""
 }
-func main() {
-	fmt.Print(LineByNum("task2/literature.txt", 5), "\n")
-}
+
+// func main() {
+// 	fmt.Print(LineByNum("task2/literature.txt", 5), "\n")
+// }
