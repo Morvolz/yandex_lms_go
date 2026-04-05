9805c75d7735e2d1c44fce1c153d99c743c5b6db
Morvolz <kola311@yandex.by>
2026-04-05 20:11:03 +0300

    TL

---
+++ Go2/level1/step5/task1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+)
+
+func Contains(ctx context.Context, r io.Reader, seq []byte) (bool, error) {
+	data := []byte{}
+	chunk := make([]byte, 2048)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return false, ctx.Err()
+
+		default:
+		}
+		n, err := r.Read(chunk)
+
+		if err != nil {
+			if err == io.EOF {
+				return false, nil
+			} else {
+				return false, err
+			}
+		}
+
+		data = append(data, chunk[:n]...)
+		if bytes.Index(data, seq) >= 0 {
+			return true, nil
+		}
+	}
+}
